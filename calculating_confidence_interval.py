87670ce9b613e254055268a6e467a2dbf8627079
xy2dxdy <sashamilto3@gmail.com>
2024-03-05 20:41:25 +0300

    creating calculating confidence interval

---
+++ calculating_confidence_interval.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+from main import generate_slots, calculate_slots_win, print_slot, count_scatters, FreeSpins, SymbolsCost
+
+if __name__ == '__main__':
+    FreeSpinsCount = 0
+    TotalMoney = 0
+    corner = int(input("Введите количество спинов: "))
+    stdDev = float(input("Введите среднеквадратичное отклонение: "))
+    for i in range(0, corner):
+        if FreeSpinsCount > 0:
+            FreeSpinsCount -= 1
+            corner += 1
+        generate_slots()
+        CurrentRollMoney, WinningLines = calculate_slots_win()
+        print_slot(WinningLines)
+        count_scat = count_scatters()
+        FreeSpinsCount += FreeSpins[count_scat]
+        CurrentRollMoney += SymbolsCost['MAP'][count_scat]
+        TotalMoney += CurrentRollMoney
+    left = float(TotalMoney) / float(corner) - 1.95 * stdDev/sqrt(corner)
+    right = float(TotalMoney) / float(corner) + 1.95 * stdDev/sqrt(corner)
+    print("[" + str("%.10f" % left) + " , " + str("%.10f" % right) + "]")
